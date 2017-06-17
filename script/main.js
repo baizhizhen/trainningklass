ce7c4b06b58eaec521b27eaa5549378dad02c306
baizhizhen <bzz_xd@sina.com>
2017-06-17 20:42:40 +0800

    change main.js

--- script/main.js
+++ script/main.js
@@ -1,9 +1,3 @@
-// require.config({
-// 	paths:{
-// 		jquery:'../lib/jquery-3.2.1.min.js'
-// 	}
-// });
-
 require(['math','string'],function (math,string) {
 	console.log(math.add(2,2));
 	console.log(string.split('jason_bai','_'));
